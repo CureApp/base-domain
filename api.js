ffa8ac88f7563041255081934100432ddeffea53
CircleCI <circleci@example.com>
2017-09-12 07:40:28 +0000

    gh-pages

---
+++ api.js
@@ -0,0 +1,47 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AggregateRoot",
+        "Base",
+        "BaseAsyncRepository",
+        "BaseDict",
+        "BaseFactory",
+        "BaseList",
+        "BaseModel",
+        "BaseModule",
+        "BaseRepository",
+        "BaseService",
+        "BaseSyncRepository",
+        "Collection",
+        "DomainError",
+        "Entity",
+        "EntityCollectionIncluder",
+        "EntityPool",
+        "Facade",
+        "FactoryInterface",
+        "Fixture",
+        "FixtureLoader",
+        "GeneralFactory",
+        "Includer",
+        "LocalRepository",
+        "MasterDataResource",
+        "MasterRepository",
+        "MemoryResource",
+        "ModelProps",
+        "ResourceClientInterface",
+        "RootInterface",
+        "Scope",
+        "TypeInfo",
+        "ValueObject"
+    ],
+    "modules": [
+        "base-domain"
+    ],
+    "allModules": [
+        {
+            "displayName": "base-domain",
+            "name": "base-domain"
+        }
+    ]
+} };
+});
